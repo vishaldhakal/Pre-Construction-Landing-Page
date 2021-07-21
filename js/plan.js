c3f3f52c5bb8077638178e5235ff3bd793aa05c2
Vishal Dhakal <vishaldhakal@Vishals-Air.domain.name>
2021-07-21 19:41:58 +0545

    sdfvsdf

--- js/plan.js
+++ js/plan.js
@@ -6,7 +6,9 @@ const closebtn = document.querySelector(".close");
 floorbtns.forEach((floor) => {
   floor.addEventListener("click", (e) => {
     /* console.log(e.target.parentElement.children[0].src); */
-    imageinner.src = e.target.parentElement.children[0].src;
+    imageinner.src =
+      e.target.parentElement.children[0].src ||
+      e.target.parentElement.parentElement.children[0].src;
     modall.classList.remove("d-none");
   });
 });
